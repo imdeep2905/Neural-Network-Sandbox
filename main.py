ca5a3e3bf13f5372c546ff7a19b33795b48283c5
Deep Raval <deepraval2905@gmail.com>
2020-01-29 23:13:02 +0530

    Changed For testing with MNIST Fashion

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #initial checking
+from Backend.NeuralNetwork import NN
+import tensorflow as tf
+if __name__ == "__main__":
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+    my_model=NN(layers_n=4,layers=[784,300,100,10],activ_fns=["None","elu","elu","softmax"],epochs=30)
+    my_model.connect_network(normalize=True)
+    my_model.fit(x_train,y_train)
+    my_model.train_visualize()
