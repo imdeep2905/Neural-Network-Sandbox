1222605ba3945d4b1ee52efbe66befac7d3e42fe
Deep Raval <deepraval2905@gmail.com>
2020-01-29 19:58:04 +0530

    Added import of NN

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -1,1 +1,8 @@
+#PACKAGE : BackEnd
+'''
+   class                       what it's for
+    NN         Main Backend for creating and training model
 
+'''
+#from .DataPreprocessing import .*
+from .NeuralNetwork import NN
