9d57ac786491a52b6595fb193c57acb62bb971bd
Deep Raval <deepraval2905@gmail.com>
2020-01-30 23:44:56 +0530

    Completed DataProcessor Class(Still in experiment)

--- Backend/DataPreprocessing.py
+++ Backend/DataPreprocessing.py
@@ -18,15 +18,26 @@ class DataProcessor:
             target=self.data.pop(self.data.columns[len(self.data.columns)-1])
             self.y_data=target.values
         else:
-            target=self.data.pop(data.columns[0])
+            target=self.data.pop(self.data.columns[0])
             self.y_data=target.values
         self.x_data=self.data.values
         shape=[self.x_data.shape[1],]
         return self.x_data,self.y_data,shape
     
-    def smart_preprocess(self,):
+    def smart_preprocess(self,factor=3):
+        # *** Experimental ***
         #this fn will replace missing values and convert non-numeric values to numeric
-    
+        #Dropping unnecessary cols and non numeric -> numeric
+        non_numeric_cols=self.data.select_dtypes(include="object")
+        for col in non_numeric_cols:
+            if len(self.data[col].unique())>self.data.shape[0]//factor or (self.data.shape[0]-self.data[col].count()) > self.data.shape[0]//factor:
+                self.data.drop([col],inplace=True,axis=1)
+            else:
+                self.data[col]=pd.Categorical(self.data[col]).codes
+        #Filling Missing values
+        self.data.fillna(value=self.data.median(),inplace=True)
+        return self.data.shape[1]
+        
 class DataSplitter:
     def __init__(self,name,path=".",train_r=0.9,test_r=0.1,val_r=0.0):
         self.file_path=os.path.join(path,name)
