6bd39846487574735dd3a0e5676a0f919f190b5d
Deep Raval <deepraval2905@gmail.com>
2020-01-30 22:10:14 +0530

    Changed for Testing

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 #initial checking
 from Backend.NeuralNetwork import NN
+from Backend.DataPreprocessing import DataProcessor
 import tensorflow as tf
 if __name__ == "__main__":
-    '''
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
-    my_model=NN(layers_n=4,layers=[784,300,100,10],activ_fns=["None","elu","elu","softmax"],epochs=2)
-    my_model.connect_network(normalize=True,y=28,x=28)
-    my_model.fit(x_train,y_train)
+    my_data=DataProcessor(path="C:\\Users\\Deep Raval\\Desktop",name="heart.csv")
+    x,y,s=my_data.get_xy()
+    my_model=NN(layers_n=3,layers=[12,24,1], opti_tech="Adam",lr=0.2,loss_fn="binary_crossentropy" ,activ_fns=["None","elu","sigmoid"],epochs=30)
+    my_model.connect_network(shape=s)
+    my_model.fit(x,y)
     my_model.train_visualize()
-    my_model.save_model("test")
-    '''
-    
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
-    my_model=NN()
-    my_model.load_model("test")
-    #print(my_model.model.summary())
-    #my_model.evaluate(x_test,y_test)
