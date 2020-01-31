39ec07fe753c1c312551507752c9f50cf5e88ac1
Deep Raval <deepraval2905@gmail.com>
2020-01-31 21:47:13 +0530

    Started Working on mainscreen

--- Frontend/MainApp.py
+++ Frontend/MainApp.py
@@ -2,7 +2,48 @@ import kivy
 kivy.require('1.11.1') 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.lang.builder import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.button import Button
+from kivy.graphics.instructions import Canvas
+#main.kv
+#rgba(44, 58, 71,1.0)
+Builder.load_string('''
+<MainScreen>:
+    FloatLayout:
+        canvas.before:
+            Color:
+                rgba: 0, 0, 0, 1.0 #Will change color later
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        StackLayout:
+            size_hint: 1,0.45
+            pos_hint:{"left":0,"bottom":root.width}
+            Button:
+                text: "Save Current Model"
+                size_hint: 1/3.,0.1
+            Button:
+                text: "Training Stats"
+                size_hint: 1/3.,0.1
+            Button:
+                text: "Browse traindata (*.csv)"    
+                size_hint: 1/3.,0.1
+            Button:
+                text: "Load Exsisting Model"
+                size_hint: 1/3.,0.1
+            Button:
+                text: "Report a Bug"
+                size_hint: 1/3.,0.1    
+            Button:
+                text: "Browse testdata (*.csv)"    
+                size_hint: 1/3.,0.1
+''')
+class MainScreen(FloatLayout):
+    pass
 
-class MainApp(App):
+class NNSandboxApp(App):
     def build(self):
-        return Label(text="Hello from Frontend !!!")
+        return MainScreen()
