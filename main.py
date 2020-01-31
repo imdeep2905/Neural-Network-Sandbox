441e138aaf84f95c5498b8fefdbf08cfda30d0b9
Deep Raval <deepraval2905@gmail.com>
2020-01-31 19:20:37 +0530

    Changed for testing

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #initial checking
 from Backend.NeuralNetwork import NN
-from Backend.DataPreprocessing import DataProcessor
+from Backend.DataPreprocessing import DataProcessor,DataSplitter
 import tensorflow as tf
 if __name__ == "__main__":
-    my_data=DataProcessor(path="C:\\Users\\Deep Raval\\Desktop\\TitanicKaggle",name="train.csv")
+    my_data=DataSplitter(path="C:\\Users\\Deep Raval\\Desktop",name="housing.csv")
     input_n=my_data.smart_preprocess()
-    x,y,s=my_data.get_xy(label_last=False)
-    my_model=NN(layers_n=3,layers=[input_n,12,1], opti_tech="Adam",lr=0.01,loss_fn="binary_crossentropy" ,activ_fns=["None","relu","sigmoid"],epochs=30)
+    #s,x,y=my_data.get_xy(label_last=False)
+    s,xtr,ytr,xva,yva,xte,yte=my_data.get_splitted_xy(test_r=0.2,val_r=0.1)
+    print(my_data.data.head())
+    my_model=NN(layers_n=3,layers=[input_n,5,1], opti_tech="Adam",lr=0.8,loss_fn="mse" ,activ_fns=["None","elu","relu"],epochs=30)
     my_model.connect_network(shape=s,normalize=True)
-    my_model.fit(x,y)
+    my_model.fit(xtr,ytr,xva,yva)
+    my_model.evaluate(xte,yte)
     my_model.train_visualize()
