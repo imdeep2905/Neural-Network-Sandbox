7b213c8c8dddc53e826231fc3b5db6a70b9552c0
Deep Raval <deepraval2905@gmail.com>
2020-01-31 19:37:31 +0530

    Changed for testing Frontend

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-#initial checking
-from Backend.NeuralNetwork import NN
-from Backend.DataPreprocessing import DataProcessor,DataSplitter
-import tensorflow as tf
+from Frontend.MainApp import MainApp
 if __name__ == "__main__":
-    my_data=DataSplitter(path="C:\\Users\\Deep Raval\\Desktop",name="housing.csv")
-    input_n=my_data.smart_preprocess()
-    #s,x,y=my_data.get_xy(label_last=False)
-    s,xtr,ytr,xva,yva,xte,yte=my_data.get_splitted_xy(test_r=0.2,val_r=0.1)
-    print(my_data.data.head())
-    my_model=NN(layers_n=3,layers=[input_n,5,1], opti_tech="Adam",lr=0.8,loss_fn="mse" ,activ_fns=["None","elu","relu"],epochs=30)
-    my_model.connect_network(shape=s,normalize=True)
-    my_model.fit(xtr,ytr,xva,yva)
-    my_model.evaluate(xte,yte)
-    my_model.train_visualize()
+    MainApp().run()
