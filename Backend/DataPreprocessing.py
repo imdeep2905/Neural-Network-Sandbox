ea6c5e9af310302d414ccfe3911771fe16a00f56
Deep Raval <deepraval2905@gmail.com>
2020-01-31 18:13:47 +0530

    Completed DataSplitter Class (Still in Experiment)

--- Backend/DataPreprocessing.py
+++ Backend/DataPreprocessing.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import pandas as pd 
 import os
 import numpy as np 
+from sklearn.model_selection import train_test_split
 
 class DataProcessor:
     def __init__(self,name,path="."):
@@ -22,7 +23,7 @@ class DataProcessor:
             self.y_data=target.values
         self.x_data=self.data.values
         shape=[self.x_data.shape[1],]
-        return self.x_data,self.y_data,shape
+        return shape,self.x_data,self.y_data
     
     def smart_preprocess(self,factor=3):
         # *** Experimental ***
@@ -38,13 +39,17 @@ class DataProcessor:
         self.data.fillna(value=self.data.median(),inplace=True)
         return self.data.shape[1]
         
-class DataSplitter:
-    def __init__(self,name,path=".",train_r=0.9,test_r=0.1,val_r=0.0):
-        self.file_path=os.path.join(path,name)
-        self.data=pd.read_csv(self.file_path)
+class DataSplitter(DataProcessor):
+    #this class is inherited because all basic funcs are same escept this will split data
+    def __init__(self, name, path='.'):
+        super().__init__(name, path=path)
+        
+    def get_splitted_xy(self,test_r=0.1,val_r=0.0,label_last=True,seed=42):
+        shape,x_data,y_data=super().get_xy(label_last=label_last)
+        x_train,x_test,y_train,y_test=train_test_split(data, labels, test_size=test_r, random_state=seed)
+        x_test,x_val,y_test,y_val=train_test_split(x_test, y_test, test_size=val_r,random_state=seed)
+        if val_r!=0:
+            return shape,x_train,y_train,x_val,y_val,x_test,y_test
+        else:
+            return shape,x_train,y_train,x_test,y_test
     
-    def get_xy(self,label_cnt=1,label_last=True):
-        pass
-    
-    def smart_preprocess(self):
-        pass
