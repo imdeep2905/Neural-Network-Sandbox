b015c00a0c49f2de9062342a5a38fb013a133bcf
Deep Raval <deepraval2905@gmail.com>
2020-02-02 13:58:18 +0530

    Added More Features

--- Frontend/MainApp.py
+++ Frontend/MainApp.py
@@ -11,24 +11,49 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.togglebutton import ToggleButton
 from kivy.graphics.instructions import Canvas
 from kivy.graphics import *
 from kivy.uix.filechooser import FileChooser
+from kivy.uix.checkbox import CheckBox
 import webbrowser
+from PIL import Image
 
 class MainScreen(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.epochs=1
     def open_tutorial(self):
         webbrowser.open("https://github.com/imdeep2905/Neural-Network-Sandbox/blob/master/README.md")
     def open_help(self):
         webbrowser.open("https://github.com/imdeep2905/Neural-Network-Sandbox/blob/master/HELP.md")
     def report_bug(self):
         webbrowser.open("https://github.com/imdeep2905/Neural-Network-Sandbox/issues/newa")
-    def get_file(self,Train=False,Test=False):
+    def get_csv_file(self,Train=False,Test=False):
         root = tk.Tk()
         root.withdraw()
-        print(Train,Test,filedialog.askopenfilename(filetypes=[('CSV Files','*.csv')]) )        
-
+        print(Train,Test,filedialog.askopenfilename(filetypes=[('CSV File','*.csv')]) )        
+    def get_h5_file(self):
+        root = tk.Tk()
+        root.withdraw()
+        print(filedialog.askopenfilename(filetypes=[('HDF5 File','*.h5')]) )
+    def show_img(self,Train=False,Test=False):
+        if Train:
+            im = Image.open('train_history_img.jpg')
+            im.show()    
+        if Test:
+            im = Image.open('test_history_img.jpg')
+            im.show()    
+    def change_epoch(self,Incr=False,Decr=False):
+        val=int(self.ids.epochs.text)
+        if Incr:
+            self.ids.epochs.text=str(val+1)
+        if Decr:
+            self.ids.epochs.text=str(max(1,val-1))
+    def temp(self,text):
+        print(text)
+        
 class NNSandboxApp(App):
     def build(self):
         return MainScreen()
