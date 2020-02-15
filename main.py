02a0e5a18abacdd9ee0bd65bb66334f1dcdc3fcd
Deep Raval <deepraval2905@gmail.com>
2020-02-15 23:43:51 +0530

    Bypassed  exception handling for Debugging

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from kivy.uix.popup import Popup
 from Frontend.MainApp import NNSandboxApp
 from kivy.uix.label import Label
-       
+'''    
 if __name__ == "__main__":
     try:
         NNSandboxApp().run()
     except Exception as inst:
         text=str(str(type(inst))+"\n"+str(inst.args)+"\n"+"You can report this using report bug button")
+        print(text)
         popup = Popup(title='Exception Occured', size_hint=(0.5, 0.5),auto_dismiss=True)
         popup.open()
         popup.add_widget((Label(text=text)))
+'''        
+        
+if __name__ == "__main__":
+    NNSandboxApp().run()        
