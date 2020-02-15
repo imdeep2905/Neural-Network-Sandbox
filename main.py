2a79305e26b8bf115b45bec279e815ea78f095a8
Deep Raval <deepraval2905@gmail.com>
2020-02-15 22:04:08 +0530

    Now exception can be catched within main

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from kivy.uix.popup import Popup
 from Frontend.MainApp import NNSandboxApp
+from kivy.uix.label import Label
+       
 if __name__ == "__main__":
-    NNSandboxApp().run()
+    try:
+        NNSandboxApp().run()
+    except Exception as inst:
+        text=str(type(inst))+"\n"+inst.args+"\n"+"You can report this using report bug button" 
+        popup = Popup(title='Exception Occured', size_hint=(0.5, 0.5),auto_dismiss=True)
+        popup.open()
+        popup.add_widget((Label(text=text)))
