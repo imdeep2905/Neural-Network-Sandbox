09de9fd3e01fcb47f9e3e12673bc1df4d7584211
Deep Raval <deepraval2905@gmail.com>
2020-02-22 22:43:09 +0530

    Tweaks for building executable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os","tensorflow"]}
+build_exe_options = {"packages": ["h5py"]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
