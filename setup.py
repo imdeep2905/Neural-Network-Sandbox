cfb9ada3dadc65edb3f3328d826d380466122ff8
Deep Raval <deepraval2905@gmail.com>
2020-02-22 16:27:16 +0530

    For creating '.exe' file

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os","tensorflow"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "Neural Network Sandbox",
+        version = "1.0.0",
+        description = "GUI based application to create and manipulate feed forward neural networks.",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("main.py", base=base)])
